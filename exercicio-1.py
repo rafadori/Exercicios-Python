e60580101358dccf02e7d007dd54bccb106eacf9
rafadori <rafadori26@gmail.com>
2022-07-21 16:20:00 -0300

    Exercicios Aula 1

---
+++ exercicio-1.py
@@ -0,0 +1,5 @@
+nome = input('Insira seu nome: ')
+idade = input('Insira sua idade: ')
+peso = input('Insira seu peso: ')
+sexo = input('Insira seu sexo: ')
+print('Boa tarde {} \nVocê tem {} anos, pesa {} kilos e seu sexo é {}'.format(nome, idade, peso, sexo))
---
+++ exercicio-2.py
@@ -0,0 +1,5 @@
+print('insira dois numeros para soma')
+n1 = int(input('Primeiro valor: '))
+n2 = float(input('Segundo valor: '))
+
+print(n1 + n2)

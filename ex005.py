70512fc5cd64faf2a122dedc86ccfd2f222d83a2
rafadori <rafadori26@gmail.com>
2022-07-22 12:08:03 -0300

    soma valores

---
+++ ex005.py
@@ -0,0 +1,7 @@
+#crie um programa que leia dois numeros e mostre a soma entre eles
+print('Programa de soma')
+n1 = int(input('Digite o primeiro numero: '))
+n2 = int(input('Digite o segundo numero: '))
+soma = n1 + n2
+print('Calculando soma...')
+print('A soma de {} com {} é igual a {}'.format(n1, n2, soma))

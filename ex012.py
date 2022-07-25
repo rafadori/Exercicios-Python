5acaa999eddefe9d2c738a85aebf5d59528a4b4b
rafadori <rafadori26@gmail.com>
2022-07-25 15:09:42 -0300

    conversor de moedas

---
+++ ex012.py
@@ -0,0 +1,8 @@
+#conversor de moedas
+
+reais = float(input('Insira aqui quantos reais você tem na carteira: R$ '))
+dolar = reais / 5.40
+print('{} reais equivalem a {:.2f} dólares'.format(reais, dolar))
+
+
+    

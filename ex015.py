2aba26ec5b96fa7aead7b1c25817af13dd4fa1fa
rafadori <rafadori26@gmail.com>
2022-07-25 16:21:12 -0300

    reajuste salário

---
+++ ex015.py
@@ -0,0 +1,5 @@
+#reajuste salarial
+
+salario = float(input('Insira o salário do funcionário: '))
+aumento = salario * 1.15
+print('O salário era de R${:.2f} e após aumento de 15% passou a ser R${:.2f} '.format(salario, aumento))

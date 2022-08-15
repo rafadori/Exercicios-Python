10182094cad16acc3f76578f2aca65b234149e46
rafadori <rafadori26@gmail.com>
2022-08-15 00:38:47 -0300

    comparando numeros

---
+++ mundo2/ex040.py
@@ -0,0 +1,11 @@
+#comparando numeros
+
+n1 = float(input('Digite o primeiro valor: '))
+n2 = float(input('Digite o segundo valor: '))
+
+if n1 > n2:
+    print('O primeiro valor é maior')
+elif n1 < n2:
+    print('O segundo valor é maior')
+else:
+    print('Os valores são iguais')

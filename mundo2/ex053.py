cb4228708e3c047fea417428914f0833da1774da
rafadori <rafadori26@gmail.com>
2022-08-29 14:04:10 -0300

    palindromo

--- mundo2/ex053.py
+++ mundo2/ex053.py
@@ -1,6 +1,10 @@
 #crie um programa que leia uma frase qualquer e diga se ela é um palindromo, desconsiderando espaços
 
-frase = str(input('Digite sua frase aqui: ')).strip().lower()
-print(frase, frase[::-1])
+frase = str(input('Digite sua frase aqui: ')).strip().lower().replace(' ', '')
+if frase == frase[::-1]:
+    print('É palindromo')
+else:
+    print('Não é palindromo')
+#print(frase, frase[::-1])
 
 #inverter: [::-1]
